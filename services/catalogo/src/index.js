fdcf4ec4f8b227597e66faf7f7accc78b2c4ccb2
Gabriel Rossi <gabrielrossi.2908@gmail.com>
2025-09-05 20:52:56 -0300

    index commit 1

---
+++ services/catalogo/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express(); 
+const PORT = process.env.PORT || 3000; 
+
+app.use(express.json());
+
+app.use('/api', routes);
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: "UP", timestamp: new Date() });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor do catálogo de cartas rodando na porta ${PORT}`);
+});
+
