e4c011814e243c75f24cd313349efa251538f214
hyocas <leandro.andrade@alunos.ifsuldeminas.edu.br>
2025-11-22 17:04:16 -0300

    Adicionado as bibliotecas helmet e axios-retry para aumento de segurança e tolerâncias as falhas da aplicação.

--- services/carrinho/src/index.js
+++ services/carrinho/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet'); 
 require('dotenv').config();
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
--- services/usuarios/src/index.js
+++ services/usuarios/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors'); 
+const helmet = require('helmet');
 const routes = require('./routes');
 
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
