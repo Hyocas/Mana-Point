ed293af7fe1c3c37f75598e183430d81874cc657
hyocas <leandro.andrade@alunos.ifsuldeminas.edu.br>
2025-11-26 11:37:31 -0300

    Adicionado os serviços grafana + prometheus - para monitoriamento.

--- services/carrinho/src/index.js
+++ services/carrinho/src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet'); 
+const client = require('prom-client');
 require('dotenv').config();
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
 
 app.use(helmet());
 app.use(cors());
@@ -14,6 +18,15 @@ app.use(express.json());
 app.use('/api', routes);
 app.get('/health', (req, res) => {
     res.status(200).json({ status: "UP", timestamp: new Date() });
+});
+
+app.get('/metrics', async (req, res) => {
+    res.set('Content-type', client.register.contentType);
+    try {
+        res.send(await client.register.metrics());
+    } catch (ex) {
+        res.status(500).send(ex);
+    }
 });
 
 app.listen(PORT, () => {
--- services/pagamento/src/index.js
+++ services/pagamento/src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');  
+const helmet = require('helmet');
+const client = require('prom-client');  
 const routes = require('./routes');
 
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
 
 app.use(helmet());
 app.use(cors());
@@ -14,6 +18,16 @@ app.use('/api', routes);
 app.get('/health', (req, res) => {
     res.status(200).json({ status: "UP", timestamp: new Date() });
 });
+
+app.get('/metrics', async (req, res) => {
+    res.set('Content-type', client.register.contentType);
+    try {
+        res.send(await client.register.metrics());
+    } catch (ex) {
+        res.status(500).send(ex);
+    }
+});
+
 
 app.listen(PORT, () => {
     console.log(`Servidor do pagamento de cartas rodando na porta ${PORT}`);
