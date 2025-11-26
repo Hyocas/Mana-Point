22d95e22b521122a8a548e63c1fb485cfa5e309e
hyocas <leandro.andrade@alunos.ifsuldeminas.edu.br>
2025-11-26 11:35:17 -0300

    Adicionado os serviços grafana + prometheus - para monitoriamento.

--- services/usuarios/src/app.js
+++ services/usuarios/src/app.js
@@ -3,9 +3,13 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const client = require('prom-client');
 const routes = require('./routes');
 
 const app = express();
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
 
 app.use(helmet());
 app.use(cors());
@@ -15,6 +19,15 @@ app.use('/api', routes);
 
 app.get('/health', (req, res) => {
     res.status(200).json({ status: "UP", timestamp: new Date() });
+});
+
+app.get('/metrics', async (req, res) => {
+    res.set('Content-type', client.register.contentType);
+    try {
+        res.send(await client.register.metrics());
+    } catch (ex) {
+        res.status(500).send(ex);
+    }
 });
 
 module.exports = app;
