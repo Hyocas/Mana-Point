ef3c4ad04c1217a3530db034a80218d98e876874
YagoArmand <omegag287@gmail.com>
2025-12-02 20:40:19 -0300

    Correção de erro encontrado com testes

--- services/carrinho/src/routes.js
+++ services/carrinho/src/routes.js
@@ -88,7 +88,7 @@ router.post('/carrinho', validarToken, async (req, res) => {
             const updateQuery = 'UPDATE carrinho_itens SET quantidade = $1, preco_unitario = $2 WHERE id = $3 RETURNING *';
             resultCarrinho = await client.query(updateQuery, [novaQuantidade, preco_unitario, existingItem.id]);
         } else { 
-            const insertQuery = 'INSERT INTO carrinho_itens (usuario_id, produto_id, quantidade, preco_unitario) VALUES ($1, $2, $3, $4) RETURNING *';
+            const itemExistenteQuery = 'SELECT id, quantidade FROM carrinho_itens WHERE usuario_id = $1 AND produto_id = $2 FOR UPDATE';
             resultCarrinho = await client.query(insertQuery, [usuarioId, produto_id, quantidade, preco_unitario]);
         }
 
