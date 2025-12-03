952e1b25d7055a92aaf533bc54bfa06b622d5ea0
hyocas <leandro.andrade@alunos.ifsuldeminas.edu.br>
2025-12-03 15:43:08 -0300

    Preparação para subir a aplicação para aws.

--- frontend/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="main-footer">
-      <p>&copy; {new Date().getFullYear()} Mana-Point Card Games. Todos os direitos reservados.</p>
-    </footer>
-  );
-}
