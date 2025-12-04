80cdde52f5f80d8fe052f96a1c202acc19c90f0f
YagoArmand <omegag287@gmail.com>
2025-12-04 20:27:36 -0300

    Testando maneira de importação do retry mais segura

--- services/catalogo/src/services/catalogoService.js
+++ services/catalogo/src/services/catalogoService.js
@@ -3,7 +3,19 @@ const axios = require('axios');
 const axiosRetry = require('axios-retry').default;
 
 const apiClient = axios.create({ timeout: 5000 });
-axiosRetry(apiClient, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
+
+function safeRetry() {
+  if (axiosRetry && typeof axiosRetry === "function") {
+    axiosRetry(apiClient, {
+      retries: 3,
+      retryDelay:
+        axiosRetry.exponentialDelay ??
+        ((retryCount) => Math.pow(2, retryCount) * 1000),
+    });
+  }
+}
+
+safeRetry();
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
