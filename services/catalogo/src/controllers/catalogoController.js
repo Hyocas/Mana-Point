5bfeb9ef14b9860c2796f9f445cfacabd724cc02
YagoArmand <omegag287@gmail.com>
2025-12-04 12:30:27 -0300

    teste mudança token

--- services/catalogo/src/controllers/catalogoController.js
+++ services/catalogo/src/controllers/catalogoController.js
@@ -52,7 +52,7 @@ module.exports = {
 
   async adicionar(req, res) {
     const tokenHeader = req.headers.authorization;
-    const token = tokenHeader ? tokenHeader.split(' ')[1] : null;
+    const token = tokenHeader.split(' ')[1];
     const { nome, quantidade } = req.body;
 
     try {
