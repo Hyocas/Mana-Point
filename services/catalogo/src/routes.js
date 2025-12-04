47748a7bdf002cb89bc0ba21b0c521aaebe03369
YagoArmand <omegag287@gmail.com>
2025-12-04 12:20:46 -0300

    Mudanças no routes

--- services/catalogo/src/routes.js
+++ services/catalogo/src/routes.js
@@ -2,27 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const catalogoController = require('./controllers/catalogoController');
-const jwt = require('jsonwebtoken');
-
-const verificarToken = (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-
-    if (!token) {
-        return res.status(401).json({ message: "Acesso negado." });
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.usuario = decoded; 
-        next();
-    } catch (error) {
-        return res.status(400).json({ message: "Token inválido." });
-    }
-};
 
 // Adicionar carta pelo nome (apenas usuário autenticado)
-router.post('/cartas', verificarToken, catalogoController.adicionar);
+router.post('/cartas', catalogoController.adicionar);
 
 // Buscar por nome ou efeito (livre)
 router.get('/cartas/search', catalogoController.buscar);
@@ -37,9 +19,9 @@ router.get('/cartas', catalogoController.listar);
 router.get('/cartas/:id', catalogoController.buscarPorId);
 
 // Remover todas as cartas (apenas autenticado)
-router.delete('/cartas', verificarToken, catalogoController.apagarCatalogo);
+router.delete('/cartas', catalogoController.apagarCatalogo);
 
 // Remover carta por ID (apenas autenticado)
-router.delete('/cartas/:id', verificarToken, catalogoController.apagarPorId);
+router.delete('/cartas/:id', catalogoController.apagarPorId);
 
 module.exports = router;
